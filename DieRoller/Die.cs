6e7caa5f3a3345333bb32f495b5eed388ffe6873
Justin-F5972 <justinfernandez27@outlook.com>
2022-01-18 22:56:52 -0800

    Basic die class made

--- DieRoller/Die.cs
+++ DieRoller/Die.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace DieRoller
 {
+    /// <summary>
+    /// Represents a single six sided die(1 - 6)
+    /// </summary>
     internal class Die
     {
+        /// <summary>
+        /// The current face up value of the die
+        /// </summary>
+        public byte FaceValue { get; set; }
+
+        /// <summary>
+        /// True if the die is currently held
+        /// </summary>
+
+        public bool IsHeld { get; set; }
+        // Face up value (What was rolled?)
+        // Is the die held?
+
+        /// <summary>
+        /// Rolls the die and sets the <see cref="FaceValue"/>
+        /// to the new number
+        /// </summary>
+        /// <returns>Returns the new random number</returns>
+        public void Roll()
+        {
+            // Generate random number
+            // Set to face value
+            throw new NotImplementedException();
+        }
     }
 }
